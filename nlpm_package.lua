61b44e730a843bcffeece1e6da388cba8cde1761
kmafeni04 <komemafeni944@gmail.com>
2026-01-10 10:37:49 +0100

    Project Init

---
+++ nlpm_package.lua
@@ -0,0 +1,14 @@
+---@class PackageDependency
+---@field name string package name as it will be used in file gen
+---@field repo string git repo
+---@field version? string git hash(#) or tag(v), defaults to "#HEAD"
+
+---@class Package
+---@field dependencies? PackageDependency[] List of package dependencies
+---@field scripts? table<string, string> scripts that can be called with `nlpm script`
+
+---@type Package
+return {
+  dependencies = {},
+  scripts = {}
+}
